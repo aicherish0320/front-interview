14a88a9214c018dcc8827075eba1382867fbcb03
aicherish <aicherish0320@163.com>
2022-05-07 21:25:25 +0800

    找出和为 n 的两个元素（循环）

--- data-structures/src/index.ts
+++ data-structures/src/index.ts
@@ -1,29 +1,25 @@
-/*
-  给一个数组，找出其中和为 n 的两个元素
-*/
-const arr = [0, 1, 3, 2, 7, 2, 5, 4]
+/**
+ 给一个数组，找出其中和为 n 的两个元素
+  有一个递增的数组 [1, 2, 4, 7, 11, 15] 和一个 n = 15
+  数组中有两个数，和是 n，即 4 + 11 === 15
+  写一个 JS 函数，找出这两个数
+思路：
+  嵌套循环，找到一个数，然后去遍历下一个数，求和，判断
+  时间复杂度是 O(n ^ 2)，不可用
+ */
 
-// ! 解法一：暴力循环
-// const twoSum = (arr: number[], target: number) => {
-//   for (let i = 0; i < arr.length; i++) {
-//     for (let j = i + 1; j < arr.length; j++) {
-//       if (arr[i] + arr[j] === target) {
-//         console.log(arr[i], arr[j])
-//       }
-//     }
-//   }
-// }
-// !解法二：使用 map
-const twoSum = (arr: number[], target: number) => {
-  const map = new Map()
-  for (let i = 0; i < arr.length; i++) {
-    const other = target - arr[i]
-    if (map.has(other)) {
-      console.log(other, arr[i])
-    } else {
-      map.set(arr[i], i)
+const arr = [1, 2, 4, 7, 11, 15]
+const n = 15
+// ! 双循环
+const twoSum = (arr: number[], n: number) => {
+  const len = arr.length
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] === n) {
+        console.log(' >>> ', arr[i], arr[j])
+      }
     }
   }
 }
 
-twoSum(arr, 4)
+twoSum(arr, n)
