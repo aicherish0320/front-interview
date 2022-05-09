2b5b0f5c69a45ed9c0c809d07b9efadc26a718cd
aicherish <aicherish0320@163.com>
2022-05-09 21:25:30 +0800

    promise sample

---
+++ knowledge-depth/promise-sample.js
@@ -0,0 +1,16 @@
+// Promise 基本示例
+
+const promise = new Promise((resolve, reject) => {
+  // 这里用于兑现的承诺，可能成功，也可能失败
+  resolve(100) // 承诺达成
+  // reject(new Error('promise rejected')) // 承诺失败
+})
+
+promise.then(
+  (value) => {
+    console.log(value)
+  },
+  (error) => {
+    console.log(error)
+  }
+)
