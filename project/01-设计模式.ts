7fc4048022b692a64a2cda403d589c87adca0d41
aicherish <aicherish0320@163.com>
2022-05-15 20:46:07 +0800

    发布订阅者和观察者模式

--- project/01-设计模式.ts
+++ project/01-设计模式.ts
@@ -16,15 +16,18 @@
 /**
  * 单例模式
  */
-// class SingleTon {
-//   private constructor() {}
-//   public static getInstance(): SingleTon {
-//     return new SingleTon()
-//   }
-//   message: string = 'Hello SingleTon'
-// }
+class SingleTon {
+  private static instance: SingleTon | null = null
+  private constructor() {}
+  public static getInstance(): SingleTon {
+    this.instance = this.instance || new SingleTon()
+    return this.instance
+  }
+  message: string = 'Hello SingleTon'
+}
 
-// const s1 = SingleTon.getInstance()
-// const s2 = SingleTon.getInstance()
-// console.log('s1.message >>> ', s1.message)
-// console.log('s2.message >>> ', s2.message)
+const s1 = SingleTon.getInstance()
+const s2 = SingleTon.getInstance()
+console.log('s1.message >>> ', s1.message)
+console.log('s2.message >>> ', s2.message)
+console.log('s1 === s2 >>> ', s1 === s2)
