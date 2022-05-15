ceb4026d1978cb20b0cbd006ab6b18a932ac027c
aicherish <aicherish0320@163.com>
2022-05-15 20:36:55 +0800

    前端设计模式

---
+++ project/01-设计模式.ts
@@ -0,0 +1,30 @@
+/**
+ * 工厂模式
+ */
+// class Person {
+//   name: string
+//   constructor(name: string) {
+//     this.name = name
+//   }
+// }
+// function createPerson(name: string) {
+//   return new Person(name)
+// }
+// const p1 = createPerson('aicherish')
+// const p2 = createPerson('aic')
+
+/**
+ * 单例模式
+ */
+// class SingleTon {
+//   private constructor() {}
+//   public static getInstance(): SingleTon {
+//     return new SingleTon()
+//   }
+//   message: string = 'Hello SingleTon'
+// }
+
+// const s1 = SingleTon.getInstance()
+// const s2 = SingleTon.getInstance()
+// console.log('s1.message >>> ', s1.message)
+// console.log('s2.message >>> ', s2.message)
