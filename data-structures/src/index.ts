d3262756a55786c919508034a405cbb7f097f6d5
aicherish <aicherish0320@163.com>
2022-05-23 07:51:03 +0800

    二叉树前序遍历

--- data-structures/src/index.ts
+++ data-structures/src/index.ts
@@ -1,1 +1,51 @@
+/**
+ * # 求一个二叉搜索树的第 K 个小值
+ * ## 二叉树
+ *  - 是一棵树
+ *  - 每个节点，最多只能有 2 个子节点
+ *  - 树节点的数据结构 { value: '', left?: '', right?: '' }
+ * ## 二叉树的遍历
+ *  - 前序遍历
+ *  - 中序遍历
+ *  - 后序遍历
+ */
+
+interface ITreeNode {
+  value: number
+  left: ITreeNode | null
+  right: ITreeNode | null
+}
+
+const tree: ITreeNode = {
+  value: 1,
+  left: {
+    value: 3,
+    left: {
+      value: 2,
+      left: null,
+      right: null
+    },
+    right: {
+      value: 5,
+      left: null,
+      right: null
+    }
+  },
+  right: {
+    value: 7,
+    left: {
+      value: 6,
+      left: null,
+      right: null
+    },
+    right: {
+      value: 4,
+      left: null,
+      right: null
+    }
+  }
+}
+
+const preOrderTraverse = (node: ITreeNode) => {}
+
 export {}
