e1ba3776d904b5cd4ea7b0e6e36415351d466830
aicherish <aicherish0320@163.com>
2022-06-15 08:00:42 +0800

    手写 new 上

---
+++ high-quality-code/04-new.js
@@ -0,0 +1,22 @@
+// new 一个对象发生了什么？手写实现？
+
+/*
+  new 一个对象的过程
+  - 创建一个空对象，继承构造函数的原型
+  - 指向构造函数（将 obj 作为 this）
+  - 返回 obj
+*/
+
+const Person = (fullName) => {
+  this.fullName = fullName
+}
+
+const person = new Person('aicherish')
+
+const acNew = (ctor) => {
+  // 创建一个实例 返回
+  const obj = Object.create()
+  // this 指向当前构造函数
+
+  return obj
+}
