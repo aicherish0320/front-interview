9218596f2cbea638e5ca5c3adf92fbbabd54d735
aicherish <aicherish0320@163.com>
2022-06-16 22:11:05 +0800

    函数科里化

--- high-quality-code/10-科里化.js
+++ high-quality-code/10-科里化.js
@@ -1,3 +1,26 @@
 /**
  * 函数科里化
  */
+
+const curry = (fn) => {
+  const newFn = (...args) => {
+    if (fn.length === args.length) {
+      return fn(...args)
+    } else {
+      return (...arg) => {
+        return newFn(...arg, ...args)
+      }
+    }
+  }
+  return newFn
+}
+
+const add = (a, b, c) => {
+  return a + b + c
+}
+
+const newAdd = curry(add)
+
+console.log(newAdd(1)(2, 3))
+console.log(newAdd(1)(2)(3))
+console.log(newAdd(1, 2, 3))
