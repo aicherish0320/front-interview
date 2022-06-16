c88598267545e2bdb5e41b6da1048006c01c014b
aicherish <aicherish0320@163.com>
2022-06-16 21:55:07 +0800

    instanceof

---
+++ high-quality-code/09-instanceof.js
@@ -0,0 +1,38 @@
+/**
+ * 手写 instanceof
+ *
+ * a instanceof A
+ * 表示在 a 的原型上找到 A
+ * 顺着 a.__proto__ 向上查找 （原型链）
+ * 看能否找到 A.prototype
+ *
+ * 值类型 instanceof 都是为 false
+ */
+
+class Person {
+  constructor(name) {
+    this.name = name
+  }
+}
+class Dog {
+  constructor(name) {
+    this.name = name
+  }
+}
+
+const p1 = new Person('jack')
+const d1 = new Dog('yellow')
+
+const acInstanceof = (inst, ctor) => {
+  if (typeof inst !== 'object' && typeof inst !== 'function') return false
+
+  while (ctor.prototype && inst) {
+    if (ctor.prototype === inst.__proto__) {
+      return true
+    }
+    inst = inst.__proto__
+  }
+
+  return false
+}
+console.log(acInstanceof(p1, Person))
