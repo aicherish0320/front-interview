8a6f0944a75ad711d041eeae36aa332c412b3af8
aicherish <aicherish0320@163.com>
2022-06-17 07:42:07 +0800

    call 实现

--- high-quality-code/12-call-apply.js
+++ high-quality-code/12-call-apply.js
@@ -1,17 +1,29 @@
 /**
  * 手写 call apply
+ *
+ * 分析：如何在函数执行时绑定 this
+ * const obj = { x: 100, fn(){ this.x } }
+ * 执行 obj.fn()，此时 fn 内部的 this 就指向 obj
+ * 可借此来实现函数绑定 this
  */
 
-Function.prototype.acCall = function () {
+Function.prototype.acCall = function (context, ...args) {
   const fn = this
+  const obj = {
+    ...context,
+    args,
+    fn
+  }
+  return obj.fn(...args)
 }
 Function.prototype.acApply = function () {}
 
-const fn = (a, b) => {
+function fn(a, b) {
+  console.log(this)
   console.log(this.foo, a + b)
 }
 const obj = {
   foo: 'foo'
 }
 
-fn.call(obj, 1, 2)
+fn.acCall(obj, 1, 2)
