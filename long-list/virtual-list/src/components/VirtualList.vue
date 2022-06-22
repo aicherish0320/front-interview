b3d61a22b21dd1a5412a525f715a04d541f71f70
aicherish0320 <aicherish0320@163.com>
2022-06-22 18:01:53 +0800

    虚拟列表中

--- long-list/virtual-list/src/components/VirtualList.vue
+++ long-list/virtual-list/src/components/VirtualList.vue
@@ -1,10 +1,13 @@
 <template>
   <!-- 能滚动的盒子 -->
-  <div class="viewport" ref="viewport">
+  <div class="viewport" ref="viewport" @scroll="handleScroll">
     <!-- 自己做一个滚动条 -->
     <div class="scroll-bar" ref="scrollBar"></div>
     <!-- 真实渲染的内容 -->
-    <div class="scroll-list">
+    <div
+      class="scroll-list"
+      :style="{ transform: `translate3d(0, ${this.offset}px,0)` }"
+    >
       <div v-for="item in visibleLists" :key="item.id">
         <slot :item="item"></slot>
       </div>
@@ -19,17 +22,36 @@ export default {
   data() {
     return {
       start: 0,
-      end: this.remain
+      end: this.remain,
+      offset: ''
     }
   },
   computed: {
     visibleLists() {
-      return this.lists.slice(this.start, this.end)
+      let start = this.start - this.prevCount
+      let end = this.end + this.nextCount
+      return this.lists.slice(start, end)
+    },
+    prevCount() {
+      return Math.min(this.start, this.remain)
+    },
+    nextCount() {
+      return Math.min(this.remain, this.lists.length - this.end)
     }
   },
   mounted() {
     this.$refs.viewport.style.height = this.size * this.remain + 'px'
     this.$refs.scrollBar.style.height = this.size * this.lists.length + 'px'
+  },
+  methods: {
+    handleScroll() {
+      // 算出来当前滚过去几个，当前应该从第几个开始显示
+      const scrollTop = this.$refs.viewport.scrollTop
+      this.start = Math.floor(scrollTop / this.size)
+      this.end = this.start + this.remain
+      // 显示区的偏移量 定位当前可视区域
+      this.offset = this.start * this.size - this.size * this.prevCount
+    }
   }
 }
 </script>
