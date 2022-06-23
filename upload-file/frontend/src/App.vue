ebbe89e73f8a7a3c91908ac69f0541061ff1ad12
aicherish0320 <aicherish0320@163.com>
2022-06-23 21:23:26 +0800

    hook 抽离

--- upload-file/frontend/src/App.vue
+++ upload-file/frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted, ref } from 'vue'
 import { useUploadFile } from './hooks/useUploadFile'
+import { useImageType } from './hooks/useImageType'
 // 文件
 const file = ref(null)
 // 进度条
@@ -16,49 +17,13 @@ const handleFileChange = (e) => {
 }
 
 const uploadFile = async () => {
-  const ret = await isImage(file.value)
+  const ret = await useImageType(file.value)
   if (!ret) {
     alert('上传文件格式不对')
     return
   }
   const { progressVal } = await useUploadFile(file.value)
   uploadProgress.value = progressVal.value
-}
-
-const isImage = async (file) => {
-  return (await isGif(file)) || (await isPng(file)) || (await isJpg(file))
-}
-const isGif = async (file) => {
-  // '47 49 46 38 39 61'(GIF89a) 或者 '47 49 46 38 37 61'(GIF87a)
-  const ret = await blobToString(file.slice(0, 6))
-  return ['47 49 46 38 39 61', '47 49 46 38 37 61'].includes(ret)
-}
-const isPng = async (file) => {
-  // '89 50 4E 47 0D 0A 1A 0A'(GIF89a)
-  const ret = await blobToString(file.slice(0, 8))
-  return ['89 50 4E 47 0D 0A 1A 0A'].includes(ret)
-}
-const isJpg = async (file) => {
-  const start = await blobToString(file.slice(0, 2))
-  const tail = await blobToString(file.slice(-2, file.size))
-  return start === 'FF D8' && tail === 'FF D9'
-}
-
-const blobToString = async (blob) => {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader()
-    reader.onload = () => {
-      // reader.result: GIF89a
-      const ret = reader.result
-        .split('')
-        .map((v) => v.charCodeAt())
-        .map((v) => v.toString(16).toUpperCase())
-        .map((v) => v.padStart(2, '0'))
-        .join(' ')
-      resolve(ret)
-    }
-    reader.readAsBinaryString(blob)
-  })
 }
 
 const bindEvents = () => {
